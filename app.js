e9ed2143180f800e7f9b253d67c134d1249e504e
David <poq-david@live.com>
2016-04-13 19:49:13 -0400

    few changes

--- app.js
+++ app.js
@@ -1,66 +1,48 @@
+//var http = require('http');
+//var https = require('follow-redirects').https;
+//var request = require('request');
+//var koa = require('koa');
+//var app = koa();
+//var route = require('koa-route');
+//var views = require('co-views');
+//var router = express.Router();
+//var url = require('url');
+//var staticCache = require('koa-static-cache');
+//var staticCache = require('express-static-cache');
+//var fs    = require("fs")
+ //var path = require('path');
+//var logger = require('winston');
+//var render = views(__dirname + '/views', { map: { html: 'swig' }});
+//var mime = require('mime-types');
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-var http = require('http');
-var https = require('follow-redirects').https;
-var request = require('request');
-var koa = require('koa');
-var app = koa();
-var route = require('koa-route');
-var views = require('co-views');
+var express = require('express');
 var config = require(__dirname + '/persistent-storage/options.js');
-var staticCache = require('koa-static-cache');
-var path = require('path');
-var logger = require('winston');
-var render = views(__dirname + '/views', { map: { html: 'swig' }});
-var mime = require('mime-types');
+
+var app = express(),
+    swig = require('swig'),
+    people;
+
+app.use(express.static(__dirname + '/public'));
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.set('view cache', false);
+
+swig.setDefaults({ cache:  'memory'  });
 
 
-var async = require('async');
-
-function handler(req, res) {
-    var url = convertURL(req.url);
-
-    if (okURL(url)) {
-        fs.readFile(url, function(err, data) {
-            if (err) {
-                res.writeHead(404);
-                return res.end("File not found.");
-            }
-            res.setHeader("Content-Type", mime.lookup(url));
-            res.writeHead(200);
-            res.end(data);
-        });
-    } else {
-        res.writeHead(403);
-        return res.end("Forbidden.");
-    }
-}
-
-logger.level = config.winston_log_level;
-app.use(staticCache(path.join(__dirname, 'public')));
-
-
-function *index() {
-    this.body = yield render('index' , {
-        app_title: config.app_title,
-        http_port: config.http_port,
-        http_host: config.http_host
-    });
-};
-
-app.use(route.get('/', index));
-app.use(route.get('/', function(req, res){
-    handler(reg, res);
-}));
-var server = http.createServer(app.callback());
-
+app.get('/', function (req, res) {
+    res.render('index' , {
+         app_title: config.app_title,
+         http_port: config.http_port,
+         http_host: config.http_host
+     });
+});
 
 
 var port = process.env.PORT || config.http_port;
-
-server.listen(port);
-logger.silly('Listening at port ' + config.http_port + ' ...');
-
-
-
-
-
+app.listen(port);
+console.log('Listening at port ' + config.http_port + ' ...');
