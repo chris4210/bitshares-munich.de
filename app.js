de5bb250c216cf2435b46fe6dcae0148f2404047
David <poq-david@live.com>
2016-04-13 21:42:24 -0400

    restore

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
-//var http = require('http');
-//var https = require('follow-redirects').https;
-//var request = require('request');
-//var koa = require('koa');
-//var app = koa();
-//var route = require('koa-route');
-//var views = require('co-views');
-//var router = express.Router();
-//var url = require('url');
-//var staticCache = require('koa-static-cache');
-//var staticCache = require('express-static-cache');
-//var fs    = require("fs")
- //var path = require('path');
-//var logger = require('winston');
-//var render = views(__dirname + '/views', { map: { html: 'swig' }});
-//var mime = require('mime-types');
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-var express = require('express');
+var http = require('http');
+var https = require('follow-redirects').https;
+var request = require('request');
+var koa = require('koa');
+var app = koa();
+var route = require('koa-route');
+var views = require('co-views');
 var config = require(__dirname + '/persistent-storage/options.js');
-
-var app = express(),
-    swig = require('swig'),
-    people;
-
-app.use(express.static(__dirname + '/public'));
-app.engine('html', swig.renderFile);
-
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-app.set('view cache', false);
-
-swig.setDefaults({ cache:  'memory'  });
+var staticCache = require('koa-static-cache');
+var path = require('path');
+var logger = require('winston');
+var render = views(__dirname + '/views', { map: { html: 'swig' }});
 
 
-app.get('/', function (req, res) {
-    res.render('index' , {
-         app_title: config.app_title,
-         http_port: config.http_port,
-         http_host: config.http_host
-     });
-});
+var async = require('async');
+
+logger.level = config.winston_log_level;
+app.use(staticCache(path.join(__dirname, 'public')));
+
+
+function *index() {
+    this.body = yield render('index' , {
+        app_title: config.app_title,
+        http_port: config.http_port,
+        http_host: config.http_host
+    });
+};
+
+app.use(route.get('/', index)); 
+
+var users = [];
+var server = http.createServer(app.callback());
+var io = require('socket.io')(server);
 
 
 var port = process.env.PORT || config.http_port;
-app.listen(port);
-console.log('Listening at port ' + config.http_port + ' ...');
+
+server.listen(port);
+logger.silly('Listening at port ' + config.http_port + ' ...');
+
+
+
+
+
