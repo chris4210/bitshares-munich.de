009c2d561fd0717d0c0bc86adc85f093908bc11b
David <poq-david@live.com>
2016-04-14 17:15:11 -0400

    few changes and fixes

--- app.js
+++ app.js
@@ -1,45 +1,36 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-var http = require('http');
-var https = require('follow-redirects').https;
-var request = require('request');
-var koa = require('koa');
-var app = koa();
-var route = require('koa-route');
-var views = require('co-views');
+var express = require('express');
 var config = require(__dirname + '/persistent-storage/options.js');
-var staticCache = require('koa-static-cache');
-var path = require('path');
-var logger = require('winston');
-var render = views(__dirname + '/views', { map: { html: 'swig' }});
+
+var compression = require('compression');
+var app = express(),
+    swig = require('swig'),
+    people;
+
+var oneDay = 86400000;
+
+app.use(compression());
+
+app.use(express.static(__dirname + '/public'));
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.set('view cache', false);
+
+swig.setDefaults({ cache:  'memory'  });
 
 
-var async = require('async');
-
-logger.level = config.winston_log_level;
-app.use(staticCache(path.join(__dirname, 'public')));
-
-
-function *index() {
-    this.body = yield render('index' , {
-        app_title: config.app_title,
-        http_port: config.http_port,
-        http_host: config.http_host
-    });
-};
-
-app.use(route.get('/', index)); 
-
-var users = [];
-var server = http.createServer(app.callback());
-var io = require('socket.io')(server);
+app.get('/', function (req, res) {
+    res.render('index' , {
+         app_title: config.app_title,
+         http_port: config.http_port,
+         http_host: config.http_host
+     });
+});
 
 
 var port = process.env.PORT || config.http_port;
-
-server.listen(port);
-logger.silly('Listening at port ' + config.http_port + ' ...');
-
-
-
-
-
+app.listen(port);
+console.log('Listening at port ' + config.http_port + ' ...');
